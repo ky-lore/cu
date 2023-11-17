2b5affd55fce3fe90ec695bef01f14a759c64ced
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2023-11-17 15:50:09 -0800

    initial templates and onboarding schema tasklist

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const testCall = require('./src/schemas/testApiCall.js');
+
+/* 
+
+variables needed to be defined after incoming POST:
+
+parentTaskId
+assigneesArray - advanced checking later (will come direct from incoming request for now)
+date - the unixtime reported in the incoming request
+
+*/
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
