9505dd1bc53f7f7ea3b8009447c6127694f22618
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2023-11-17 15:50:09 -0800

    initial templates and onboarding schema tasklist

---
+++ src/schemas/globalOnboardingTasks.js
@@ -0,0 +1,83 @@
+require('dotenv').config();
+
+const adminIds = process.env.ADMIN_IDS.split('_')
+
+const taskList = [
+  {
+    name: "Confirm Onboarding Form",
+    description: null,
+    markdown_description: null,
+    assignees: null,
+    status: "onboarding",
+    due_date: 28800000,
+    time_estimate: null,
+    start_date: null,
+    notify_all: true,
+    parent: null
+  },
+  {
+    name: "Pin live onboarding doc to INTERNAL Slack channel",
+    description: null,
+    markdown_description: null,
+    assignees: null,
+    status: "onboarding",
+    due_date: 28800000,
+    time_estimate: null,
+    start_date: null,
+    notify_all: true,
+    parent: null
+  },
+  {
+    name: "Introduce self to client in EXTERNAL Slack channel",
+    description: null,
+    markdown_description: null,
+    assignees: null,
+    status: "onboarding",
+    due_date: 28800000,
+    time_estimate: null,
+    start_date: null,
+    notify_all: true,
+    parent: null
+  },
+  {
+    name: "Assign all members to master ClickUp card",
+    description: null,
+    markdown_description: null,
+    assignees: adminIds,
+    status: "onboarding",
+    due_date: 28800000,
+    time_estimate: null,
+    start_date: null,
+    notify_all: true,
+    parent: null
+  },
+  {
+    name: "Assign main POCs to custom fields in ClickUp card",
+    description: null,
+    markdown_description: null,
+    assignees: adminIds,
+    status: "onboarding",
+    due_date: 28800000,
+    time_estimate: null,
+    start_date: null,
+    notify_all: true,
+    parent: null
+  },
+  {
+    name: "Schedule leads kickoff call with client",
+    description: null,
+    markdown_description: null,
+    assignees: adminIds,
+    status: "onboarding",
+    due_date: 259200000,
+    time_estimate: null,
+    start_date: null,
+    notify_all: true,
+    parent: null
+  }
+]
+
+// assignees, parent will both be applied after request
+// due_date will accept request time's unixtime added to it
+
+module.exports = taskList
---
+++ src/schemas/testApiCall.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+async function main() {
+  const apiKey = process.env.API_KEY;
+  const listId = process.env.LIST_ID;
+
+  let testTask = {
+    "name": "New Task Name",
+    "description": "New Task Description",
+    "markdown_description": "New Task Description",
+    "assignees": [
+      82219496
+    ],
+    "tags": [
+      "tag name 1"
+    ],
+    "status": "Onboarding",
+    "priority": 3,
+    "due_date": 1508369194377,
+    "due_date_time": false,
+    "time_estimate": 8640000,
+    "start_date": 1567780450202,
+    "start_date_time": false,
+    "notify_all": true,
+    "parent": null,
+    "links_to": null,
+    "check_required_custom_fields": true,
+    "custom_fields": [
+    ]
+  }
+
+  const cuPush = await fetch(
+    `https://api.clickup.com/api/v2/list/${listId}/task`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': apiKey
+      },
+      body: JSON.stringify(testTask)
+    }
+  );
+
+  const data = await cuPush.json();
+  console.log(data);
+}
+
+main();
