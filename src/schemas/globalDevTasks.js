d07f57f082dc01daaef05152af5e9c955da5c87e
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2023-11-22 14:27:55 -0800

    refactoring strat tasks

--- src/schemas/globalDevTasks.js
+++ src/schemas/globalDevTasks.js
@@ -3,70 +3,72 @@
 const hours = 3600000
 const days = 86400000
 
-const taskFlags = {
-  gbl: 'gbl',
-  srch: 'srch',
-  socl: 'socl',
-  cro: 'cro',
-  ctv: 'ctv',
-};
-
 const gblTasks = [
   {
-    name: "Schedule initial strategy meet",
-    due_date: 8 * hours
+    name: "(LEADS) Schedule team-wide pre-launch checks and QC process",
+    due_date: 5 * days,
+    flag: 'gbl'
   }
-].map(obj => ({ ..obj, flag: taskFlags.gbl }));
+]
 
 const srchTasks = [
   {
-    name: "Create assets/accounts if client does not have",
-    due_date: 2 * days,
-    flag: 'srch'
+    name: "Audience creation for campaigns",
+    due_date: 1 * days,
+    flag: 'srch',
+    tags: ['youtube']
   },
   {
-    name: "Connect with CRO to kickoff landing page builds",
+    name: "Add all exclusions",
     due_date: 2 * days,
-    flag: 'srch'
+    flag: 'srch',
+    tags: ['youtube']
   }
-].map(obj => ({ ..obj, flag: taskFlags.srch }));
+]
 
 const soclTasks = [
   {
     name: "Conduct competitor analysis",
-    due_date: 2 * days
+    due_date: 2 * days,
+    flag: 'socl'
   },
   {
     name: "Audit of past account histories",
-    due_date: 2 * days
+    due_date: 2 * days,
+    flag: 'socl'
   },
   {
     name: "Creative requests if needed",
-    due_date: 1 * days
+    due_date: 1 * days,
+    flag: 'socl'
   }
-]..map(obj => ({ ..obj, flag: taskFlags.socl }));
+]
 
 const croTasks = [
   {
     name: "Internal strategy meet",
-    due_date: 1 * days
+    due_date: 1 * days,
+    flag: 'cro'
   },
   {
     name: "Create and pin web SOP documents",
-    due_date: 8 * hours
+    due_date: 8 * hours,
+    flag: 'cro'
   },
   {
     name: "Create and pin mind map",
-    due_date: 2 * days
+    due_date: 2 * days,
+    flag: 'cro'
   }
-].map(obj => ({ ..obj, flag: taskFlags.cro }));
+]
 
 const ctvTasks = [
   {
     name: "Curate style guide if non-existent",
-    due_date: 1 * days
+    due_date: 1 * days,
+    flag: 'ctv'
   }
-].map(obj => ({ ..obj, flag: taskFlags.ctv }));
+]
 
 // Concatenating all four arrays into one final export array to send to createSubtasks
 module.exports = [...gblTasks, ...srchTasks, ...soclTasks, ...croTasks, ...ctvTasks]
