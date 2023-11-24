b6e764f52aa7bdf4878dda7cfd03930980d8644d
ky-lore <hoshikyn@gmail.com>
2023-11-23 16:42:24 -0800

    minor refactoring and documentation

--- src/utils/parseTags.js
+++ src/utils/parseTags.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @param {object} task Task object passed in by handlers
+ * @returns {array} Array of tag names
+ */
 module.exports = function (task) {
   return task.map(obj => {
     return obj.name
