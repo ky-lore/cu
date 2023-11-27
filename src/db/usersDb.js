dab53c74ecc09444fa327103c12eba2a45395fc6
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2023-11-27 15:13:46 -0800

    updating userdb, prep for test launch

--- src/db/usersDb.js
+++ src/db/usersDb.js
@@ -10,5 +10,45 @@ module.exports = [
   {
     _name: 'Kyle Lorenzo',
     uid: 5997125
+  },
+  {
+    _name: 'Chris Baker',
+    uid: 14881210
+  },
+  {
+    _name: 'Danny Pawelski',
+    uid: 82219496
+  },
+  {
+    _name: 'Darren Le',
+    uid: 44126937
+  },
+  {
+    _name: 'Michael Fances',
+    uid: 14781133
+  },
+  {
+    _name: 'Michelle Gao',
+    uid: 56419683
+  },
+  {
+    _name: 'Nate Park',
+    uid: 82219495
+  },
+  {
+    _name: 'Nestor Rodriguez',
+    uid: 26111639
+  },
+  {
+    _name: 'Patrick Lam',
+    uid: 26331210
+  },
+  {
+    _name: 'Sandra Miller',
+    uid: 26199940
+  },
+  {
+    _name: 'Victor Villeda',
+    uid: 50647944
   }
 ]
