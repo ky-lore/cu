c997d6c3334326dd6c798e42045001a506b9f493
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2023-12-20 14:47:56 +0900

    asynchronicity fixes

--- src/controllers/createSubtasks.js
+++ src/controllers/createSubtasks.js
@@ -1,24 +1,26 @@
-const axios = require('axios')
-const header = require('../../routes/_resources/header')
+const axios = require('axios');
+const header = require('../../routes/_resources/header');
 
 /**
  * 
  * @param {*} taskArray array of initialized tasks from taskHandler.js
  * @param {*} listId needed to push to corresponding CU board
  */
-function createSubtasks(taskArray, listId) {
-  console.log(taskArray)
-  // hit the CU API with each task object
-  taskArray.forEach(taskObj => {
+async function createSubtasks(taskArray, listId) {
+  console.log(taskArray);
+
+  // Use Promise.all to wait for all requests to complete
+  await Promise.all(taskArray.map(async (taskObj) => {
     const url = `https://api.clickup.com/api/v2/list/${listId}/task?custom_task_ids=true`;
 
     try {
-      axios.post(url, JSON.stringify(taskObj), header);
+      await axios.post(url, JSON.stringify(taskObj), header);
+      console.log('Subtask created successfully:', taskObj.name);
     } catch (err) {
       // TODO: Handle errors appropriately
       console.error("Error creating subtask:", err.message);
     }
-  })
+  }));
 }
 
-module.exports = createSubtasks
+module.exports = createSubtasks;
