7c19836afaa523fa8168ae0822d91fa4c015bbc1
Alexis Martin <webdeveloperlex@gmail.com>
2024-03-15 08:56:26 -0700

    fixeD

--- routes/tasks.js
+++ routes/tasks.js
@@ -15,8 +15,8 @@ router.post("/task-moved/:taskId", async (req, res) => {
 		const task = await getTaskById(taskId);
 
 		// taskHandler handles the task
-		await taskHandler(task);
 		await ghlOpportunityHandler(task);
+		await taskHandler(task);
 
 		res.send("Success");
 	} catch (error) {
