3e348ea7e8a81eede8a30bedac073cad22bee793
Alexis Martin <webdeveloperlex@gmail.com>
2024-03-15 08:43:11 -0700

    removed src/db from gitignore

---
+++ src/db/stageIdMappings.js
@@ -0,0 +1,8 @@
+module.exports = {
+	onboarding: "7aceedaa-538c-4e37-ae6f-6dcf77d7848d",
+	strategizing: "3b5b86b0-a685-4c62-9bbd-be4c66b3e449",
+	development: "7699df27-cabb-4837-97d0-bf6b95b9817a",
+	live: "1bb6729c-8fc0-47d8-aa8b-544d8aa6b85e",
+	optimizations: "5c2ce962-d409-44be-a564-79e8f5bad21f",
+	paused: "dd7651fb-2a73-4ea4-9a19-142e9d5fe2c6",
+};
