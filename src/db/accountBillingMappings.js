1cb5788d4e57e9e2b81ffe611d012d71696732c9
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-05-09 11:15:49 -0700

    initial mapping schema

---
+++ src/db/accountBillingMappings.js
@@ -0,0 +1,34 @@
+module.exports = {
+  {
+    accountName: 'Always a Student',
+    billingDate: 1
+  },
+  {
+    accountName: 'BAM Capital',
+    billingDate: 1
+  },
+  {
+    accountName: 'Elite Ideas',
+    billingDate: 25
+  },
+  {
+    accountName: 'Hemopet',
+    billingDate: 18
+  },
+  {
+    accountName: 'Safe Secure Trust',
+    billingDate: 17
+  },
+  {
+    accountName: 'Dash Athletics',
+    billingDate: 22
+  },
+  {
+    accountName: 'Zone Mouthguard',
+    billingDate: 26
+  },
+  {
+    accountName: 'Luke / Prestige Worldwide',
+    billingDate: 25
+  }
+}
--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,5 @@
 module.exports = {
-  usersDb: require('./usersDb')
+  usersDb: require('./usersDb'),
+  
   // stageIdMappings: require('./stageIdMappings')
 }
