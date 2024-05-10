c5466c1be5a5a0ab9e49674f62a9f0ee982ce64a
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-05-10 12:03:15 -0700

    slack channel mapping

--- src/db/accountBillingMappings.js
+++ src/db/accountBillingMappings.js
@@ -1,15 +1,17 @@
 module.exports = [
   {
     accountName: 'Always a Student',
-    billingDate: 1
+    billingDate: 1,
+    channelId: 'C046WEXCZ98'
   },
   {
     accountName: 'BAM Capital',
-    billingDate: 1
+    billingDate: 1,
+    channelId: 'C03GYL0SM88'
   },
   {
     accountName: 'Elite Ideas',
-    billingDate: 11
+    billingDate: 10
   },
   {
     accountName: 'Hemopet',
@@ -17,18 +19,22 @@ module.exports = [
   },
   {
     accountName: 'Safe Secure Trust',
-    billingDate: 17
+    billingDate: 17,
+    channelId: 'C06PNBKV7ME'
   },
   {
     accountName: 'Dash Athletics',
-    billingDate: 22
+    billingDate: 22,
+    channelId: 'C06LYARKPC4'
   },
   {
     accountName: 'Zone Mouthguard',
-    billingDate: 26
+    billingDate: 26,
+    channelId: 'C063NFTM92P'
   },
   {
     accountName: 'Luke / Prestige Worldwide',
-    billingDate: 25
+    billingDate: 25,
+    channelId: 'C02DE3259QB'
   }
 ]
