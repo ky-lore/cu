c8134d6ef2755a9e50465347c630c0cb9ffd6482
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-05-13 15:22:11 -0700

    testing prepstage

--- src/services/index.js
+++ src/services/index.js
@@ -5,4 +5,5 @@ module.exports = {
   handleDailies: require("./handleDailies"),
   handleLive: require("./handleLive"),
   handleLasTasks: require("./handleLasTasks"),
+  handlePreparation: require('./handlePrep')
 };
