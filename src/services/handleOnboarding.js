13997fa011ddf84749ce486348b3606f0fb9438d
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-05-14 13:34:27 -0700

    test onboarding

--- src/services/handleOnboarding.js
+++ src/services/handleOnboarding.js
@@ -3,6 +3,10 @@ const { datetime, adminIds, parseLeads, parseTags } = require('../utils')
 
 const { createSubtasks } = require('../controllers')
 
+// const testLeads = { croLead: 5997125, srchLead: 82219496, soclLead: 82242423 }
+
+// const testTags = ['social', 'leadgen', 'ecom', 'dev', 'google']
+
 /**
  * 
  * @param {object} task The original request object sent from CU passed in from our route
@@ -11,23 +15,61 @@ const { createSubtasks } = require('../controllers')
  */
 
 async function handleOnboarding(task, assigneeIds) {
-  // let listId = task.list.id
-  // let taskArray = globalOnboarding.map(obj => {
-  //   return {
-  //     ...obj,
-  //     assignees: obj.leads ? adminIds : assigneeIds,
-  //     due_date: obj.due_date + datetime(),
-  //     start_date: datetime(),
-  //     parent: task.id
-  //   }
-  // })
-  // console.log(taskArray)
-  // await createSubtasks(taskArray, listId)
+
   const leads = parseLeads(task)
   const parsedTags = parseTags(task.tags)
-  console.log(task, assigneeIds, leads, parsedTags)
 
-  
+  // console.log(testLeads, testTags)
+
+  var testArr = globalOnboarding.map(task => {
+    return {
+      ...task,
+      due_date: task.due_date + datetime(),
+      start_date: datetime(),
+      parent: 123
+    }
+  }).filter(task => {
+    return task.flags.every(element => parsedTags.includes(element))
+  }).map(task => {
+    switch(task.flags.join('-')) {
+      case 'social-ecom':
+        return {
+          ...task,
+          assignees: leads.soclLead
+        }
+        break
+      case 'social-leadgen':
+        return {
+          ...task,
+          assignees: leads.soclLead
+        }
+        break
+      case 'google-ecom':
+        return {
+          ...task,
+          assignees: leads.srchLead
+        }
+        break
+      case 'google-leadgen':
+        return {
+          ...task,
+          assignees: leads.srchLead
+        }
+        break
+      case 'dev':
+        return {
+          ...task,
+          assignees: leads.croLead
+        }
+        break
+      default:
+        return task
+    }
+  })
+
+  console.log(testArr)
 }
+
+handleOnboarding()
 
 module.exports = handleOnboarding
