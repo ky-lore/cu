6916629f7ceaeaaffbb97441dde651aae5f5d935
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-06-10 11:24:57 -0700

    endpoint for zapier

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const { assignLsa } = require("./routes/_helpers/helpers");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/tasks", require("./routes/tasks"));
 app.use("/recur", require("./routes/recur"));
 app.use("/reschedule", require("./routes/reschedule"));
 app.use("/morning", require("./routes/morning"));
 app.use("/night", require("./routes/night"));
 app.use("/billing", require("./routes/billing"));
+app.use("/clickup", require("./routes/clickup"));
 
 const port = process.env.PORT;
 
