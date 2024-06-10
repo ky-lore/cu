73c6b22b318b9268463ca514051a6df7481e49b7
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-06-10 14:34:44 -0700

    db updates

--- src/db/accountBillingMappings.js
+++ src/db/accountBillingMappings.js
@@ -1,43 +1,88 @@
+const ids = require('./ids')
+
 module.exports = [
   {
     accountName: 'Always a Student',
     billingDate: 1,
-    channelId: 'C046M7NUW3H'
+    channelId: 'C046M7NUW3H',
+    poc: ids.sandra
   },
   {
     accountName: 'BAM Capital',
-    billingDate: 1,
-    channelId: 'C03KLA3N0TZ'
+    billingDate: 12,
+    channelId: 'C03KLA3N0TZ',
+    poc: ids.abby
   },
   {
     accountName: 'Elite Ideas',
     billingDate: 25,
-    channelId: 'C90ISDJ9UF'
+    channelId: 'C90ISDJ9UF',
+    poc: ids.abby
   },
   {
     accountName: 'Hemopet',
     billingDate: 12,
     channelId: 'C028VD7P2SH',
-    poc: 'U06BQHXBY02'
+    poc: ids.michael
   },
   {
     accountName: 'Safe Secure Trust',
-    billingDate: 17,
-    channelId: 'C06NZJKC2JW'
+    billingDate: 11,
+    channelId: 'C06NZJKC2JW',
+    poc: ids.michael
   },
   {
     accountName: 'Dash Athletics',
     billingDate: 21,
-    channelId: 'C06L1JD587Q'
+    channelId: 'C06L1JD587Q',
+    poc: ids.abby
   },
   {
     accountName: 'Zone Mouthguard',
     billingDate: 26,
-    channelId: 'C02KBSHD3CL'
+    channelId: 'C02KBSHD3CL',
+    poc: ids.michael
   },
   {
-    accountName: 'Luke / Prestige Worldwide',
+    accountName: 'Cordae NY',
+    billingDate: 30,
+    channelId: 'C028YKZ98F5',
+    poc: ids.abby
+  },
+  {
+    accountName: 'Titanium Solar',
+    billingDate: 1,
+    channelId: 'C0389NX5CJY',
+    poc: ids.danny
+  },
+  {
+    accountName: 'KMD89',
+    billingDate: 1,
+    channelId: 'C028YKZ98F5',
+    poc: ids.abby
+  },
+  {
+    accountName: 'Lunasy Capital Rentals',
     billingDate: 25,
-    channelId: 'C02DP8560HK'
-  }
+    channelId: 'C06RJ3VKU5T',
+    poc: ids.nate
+  },
+  {
+    accountName: 'Nim Stant',
+    billingDate: 14,
+    channelId: 'C073FMKHH7D',
+    poc: ids.michael
+  },
+  {
+    accountName: 'Nations Insurance',
+    billingDate: 17,
+    channelId: 'C073Y5WA15K',
+    poc: ids.nate
+  },
+  {
+    accountName: 'Energyscape Renewables',
+    billingDate: 17,
+    channelId: 'C0747RQFXFZ',
+    poc: ids.abby
+  },
 ]
