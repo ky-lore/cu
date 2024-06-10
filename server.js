1d9c1da8dbba2f10cccb1eb9010da1c0b22f1dd4
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-06-10 08:51:09 -0700

    routing update

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use("/recur", require("./routes/recur"));
 app.use("/reschedule", require("./routes/reschedule"));
 app.use("/morning", require("./routes/morning"));
 app.use("/night", require("./routes/night"));
+app.use("/billing", require("./routes/billing"));
 
 const port = process.env.PORT;
 
