5685db6880adbf6f4cd20a3ac829c67541e74494
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-06-10 08:49:54 -0700

    updating billing logic to not skip weekends

---
+++ routes/billing.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const { usersDb, billingMapping } = require("../src/db");
+const { datetime } = require("../src/utils");
+const zapUrl = process.env.ZAPIER_BILLING_ENDPOINT;
+
+async function handler(billingMapping) {
+  console.log('BILLING')
+  const today = new Date().getDate();
+  console.log(today)
+  const lastDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 0).getDate();
+  
+  for (const entry of billingMapping) {
+    const billingDate = entry.billingDate;
+    console.log(entry)
+    
+    if (today === billingDate - 2 || (today === lastDayOfMonth && (billingDate === 1 || billingDate === 2))) {
+      axios.post(zapUrl, entry)
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    }
+  }
+}
+
+router.get("/", async (req, res) => {
+  try {
+    await handler(billingMapping);
+    
+    res.send("Success");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
