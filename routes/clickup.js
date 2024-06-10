89b55541b893a1268e250947672f394419940129
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-06-10 11:24:57 -0700

    endpoint for zapier

---
+++ routes/clickup.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const zapUrl = process.env.ZAPIER_CU_ENDPOINT;
+const axios = require('axios');
+
+async function handler() {
+
+}
+
+router.post("/", async (req, res) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    res.send('Data received successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
