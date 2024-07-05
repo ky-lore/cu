12060d69912e3dad1042748bedc881fcc8a30573
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-07-05 13:10:51 -0700

    zapier link

--- routes/overdue.js
+++ routes/overdue.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { datetime } = require("../src/utils");
-const zapUrl = process.env.ZAPIER_BILLING_ENDPOINT;
+const zapUrl = process.env.ZAPIER_OVERDUE_ENDPOINT;
 const axios = require('axios')
 const header = require('./_resources/header')
 
@@ -10,9 +10,14 @@ async function handler() {
   const listId = '900901670366';
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const url = `https://api.clickup.com/api/v2/list/${listId}/task?due_date_lt=${currentTimestamp}`;
-  axios.get(url, header)
+  await axios.get(url, header)
   .then((res) => {
-    console.log(res.data)
+    const tasks = res.data.tasks;
+    tasks.forEach(task => {
+      axios.post(zapUrl, task)
+      .then(res => console.log(res))
+      .catch(err => console.error(err))
+    })
   })
   .catch(err => console.error(err))
 }
