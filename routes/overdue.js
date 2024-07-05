92e40f992c17f558b40ded683cbe6430a856100d
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-07-05 12:45:47 -0700

    response data

--- routes/overdue.js
+++ routes/overdue.js
@@ -8,10 +8,34 @@ const header = require('./_resources/header')
 
 async function handler() {
   console.log('Overdue OK')
-  const url = `https://api.clickup.com/api/v2/list/${listId}/task`
+  const query = new URLSearchParams({
+    archived: 'false',
+    include_markdown_description: 'true',
+    page: '0',
+    order_by: 'string',
+    reverse: 'true',
+    subtasks: 'true',
+    statuses: 'string',
+    include_closed: 'true',
+    assignees: 'string',
+    watchers: 'string',
+    tags: 'string',
+    due_date_gt: '0',
+    due_date_lt: '0',
+    date_created_gt: '0',
+    date_created_lt: '0',
+    date_updated_gt: '0',
+    date_updated_lt: '0',
+    date_done_gt: '0',
+    date_done_lt: '0',
+    custom_fields: 'string',
+    custom_field: 'string',
+    custom_items: '0'
+  }).toString();
+  const url = `https://api.clickup.com/api/v2/list/${listId}/task?${query}`
   axios.get(url, header)
   .then((res) => {
-    console.log(res)
+    console.log(res.data)
   })
   .catch(err => console.error(err))
 }
