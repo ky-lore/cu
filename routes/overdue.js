65f066c56fb2ee3e920f02e8617dfd142f46af89
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2024-07-05 12:41:15 -0700

    overdue responsetest

--- routes/overdue.js
+++ routes/overdue.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const { datetime } = require("../src/utils");
 const zapUrl = process.env.ZAPIER_BILLING_ENDPOINT;
 const axios = require('axios')
+const listId = process.env.LIST_ID;
+const header = require('./_resources/header')
 
 async function handler() {
   console.log('Overdue OK')
+  const url = `https://api.clickup.com/api/v2/list/${listId}/task`
+  axios.get(url, header)
+  .then((res) => {
+    console.log(res)
+  })
+  .catch(err => console.error(err))
 }
 
 router.get("/", async (req, res) => {
