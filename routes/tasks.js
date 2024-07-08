aabae9b5ed5ea86ceaa04b14dc4ec9937639b931
Grafcar <bryandelossantos@Grafcar.lan>
2024-07-08 15:32:00 -0700

    First commit, testing assignee when creatinging tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,25 +9,7 @@ const taskHandler = require("../src/controllers/taskHandler");
 // @access  Public
 router.post("/task-moved/:taskId", async (req, res) => {
 	const { taskId } = req.params;
-
-	try {
-		// Retrieve the details of the task that was moved using the taskId
-		const task = await getTaskById(taskId);
-
-		// taskHandler handles the task
-		// await ghlOpportunityHandler(task);
-		await taskHandler(task);
-
-		res.send("Success");
-	} catch (error) {
-		console.error(error);
-		res.status(500).send("Internal Server Error");
-	}
-});
-
-router.post("/subtask-moved/:taskId", async (req, res) => {
-	const { taskId } = req.params;
-
+	console.log(`Received task ID:${taskId}`)
 	try {
 		// Retrieve the details of the task that was moved using the taskId
 		const task = await getTaskById(taskId);
