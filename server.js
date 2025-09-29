32d8b327b54bc4d1999f7bf94999d6a26f34661b
Kyle-Christopher Lorenzo <hoshikyn@gmail.com>
2025-09-29 15:17:57 -0700

    Stripping routes

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/tasks", require("./routes/tasks"));
-app.use("/recur", require("./routes/recur"));
-app.use("/reschedule", require("./routes/reschedule"));
-app.use("/morning", require("./routes/morning"));
-app.use("/night", require("./routes/night"));
-app.use("/billing", require("./routes/billing"));
-app.use("/clickup", require("./routes/clickup"));
-app.use("/overdue", require("./routes/overdue"));
-app.use("/clients", require("./routes/clients"));
+// app.use("/tasks", require("./routes/tasks"));
+// app.use("/recur", require("./routes/recur"));
+// app.use("/reschedule", require("./routes/reschedule"));
+// app.use("/morning", require("./routes/morning"));
+// app.use("/night", require("./routes/night"));
+// app.use("/billing", require("./routes/billing"));
+// app.use("/clickup", require("./routes/clickup"));
+// app.use("/overdue", require("./routes/overdue"));
+// app.use("/clients", require("./routes/clients"));
+app.use("/health", require("./routes/health"));
 
 
 const port = 8080;
