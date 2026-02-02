1b2803a017c605f22edb2c0347a0a6810194be6a
Gurnoor Singh <gurnos1@uci.edu>
2026-02-02 10:11:32 -0800

    Test script

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/gohighaccount", require("./routes/gohighaccount"));
 const port = 8080;
 const axios = require("axios");
 
-function empty(){
+function empty2(){
 
 }
 app.listen(port, () => {
